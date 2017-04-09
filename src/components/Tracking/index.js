aceab22d99ae86af09bc63f4a875a92a0b203cbc
Julio Gudino <julsgud@gmail.com>
2017-04-08 21:45:23 -0500

    Added face tracking with Clarifai :).

--- src/components/Tracking/index.js
+++ src/components/Tracking/index.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import tracking from 'tracking';
+import tracking from './lib/tracking.js';
+
+const 
 
 class Tracking extends React.Component {
+	static defaultProps = {
+		img: null
+	}
+
 	static state = {
 		isTrackingLoaded: false,
 		isProcessing: false
 	}
 
 	componentDidMount() {
-		if (document.getElementById(''))
+		// this.props.img = document.getElementById('i');
 	}
 
 	render() {
-		return(
+		console.log(tracking);
+		let tracker = new tracking.ObjectTracker(["face"]);
 
+		// tracker.setStepSize(1.8);
+
+		// tracking.track('#i', tracker);
+
+		// tracker.on('track', (e) => {
+		// 	e.data.forEach((r) => {
+		// 		window.plot(r.x, r.y, r.width, r.height);
+		// 	});
+		// });
+
+		// window.plot = (x, y, w, h) => {
+		// 	const rect = document.createElement('div');
+		// 	document.querySelector('#c').appendChild(rect);
+		// 	rect.classList.add('rect');
+  //       	rect.style.width = w + 'px';
+  //       	rect.style.height = h + 'px';
+  //       	rect.style.left = (img.offsetLeft + x) + 'px';
+  //       	rect.style.top = (img.offsetTop + y) + 'px';
+		// }
+
+
+		return(
+			<div></div>
 		);
 	}
 }
 
-export default tracking;
+export default Tracking;
