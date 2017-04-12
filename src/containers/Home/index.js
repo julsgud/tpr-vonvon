c16f470266e84d4bb77961011b1d4d5aba4e2aa7
Julio Gudino <julsgud@gmail.com>
2017-04-12 09:11:31 -0500

    No flexgrid test.

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -21,26 +21,47 @@ class Home extends Component {
 
 	render() {
 		return(
-			<Row center='xs'>
-				<Col xs={12}>
+			<div>
+				<div>
 					<Img src="https://res.cloudinary.com/julsgc/image/upload/v1491106020/Boletia_995x380__2_fqawa8.png"/>
-				</Col>
-				<Col xs={12}>
+				</div>
+				<div>
 					<h3> A que Ser Extraordinario te pareces!? </h3>
-				</Col>
-				<Col xs={12}>
+				</div>
+				<div>
 					<p> Averigualo ahora! </p>
-				</Col>
-				<Col>
-					{/*<FacebookLogin 
+				</div>
+				<div>
+					<FacebookLogin 
 						infoCallback={this.props.infoHandler}
 						imageCallback={(data) => {
 							this.props.imageHandler(data);
 							this.props.history.push('/select');
-						}}/>*/}
-				</Col>
-			</Row>
+						}}/>
+				</div>
+			</div>
 		);
+		// return(
+		// 	<Row center='xs'>
+		// 		<Col xs={12}>
+		// 			<Img src="https://res.cloudinary.com/julsgc/image/upload/v1491106020/Boletia_995x380__2_fqawa8.png"/>
+		// 		</Col>
+		// 		<Col xs={12}>
+		// 			<h3> A que Ser Extraordinario te pareces!? </h3>
+		// 		</Col>
+		// 		<Col xs={12}>
+		// 			<p> Averigualo ahora! </p>
+		// 		</Col>
+		// 		<Col>
+		// 			<FacebookLogin 
+		// 				infoCallback={this.props.infoHandler}
+		// 				imageCallback={(data) => {
+		// 					this.props.imageHandler(data);
+		// 					this.props.history.push('/select');
+		// 				}}/>
+		// 		</Col>
+		// 	</Row>
+		// );
 	}
 }
 
